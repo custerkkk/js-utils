54a4226eaf49ffa44d1d2c0d9cf0f43712b8c373
adam <adam.dong@baiyitech.com.tw>
2021-10-05 16:59:25 +0800

    新增 functions

--- index.ts
+++ index.ts
@@ -23,4 +23,64 @@ export module js_utils {
         }
         return fmt;
     }
+
+    /**
+     * 获取UUID
+     */
+    export function generateUUID() {
+        var d = new Date().getTime();
+        var uuid = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+            var r = (d + Math.random() * 16) % 16 | 0;
+            d = Math.floor(d / 16);
+            return (c == "x" ? r : (r & 0x3) | 0x8).toString(16);
+        });
+        return uuid;
+    }
+
+    /**
+     * 是否为移动设备
+     */
+    export function isMobile() {
+        const flag = navigator.userAgent.match(
+            /(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i
+        );
+        return flag;
+    }
+
+    /**
+     * 检测是否为json
+     * @param str
+     */
+    export function isJsonString(str: string): boolean {
+        try {
+            if (typeof JSON.parse(str) == "object") {
+                return true;
+            }
+        } catch (e) {
+            console.log("not json");
+        }
+        return false;
+    }
+    /**
+     * json字符串转对象
+     * @param str
+     */
+    export function jsonToObject(str: string): any {
+        try {
+            var obj = eval("(" + str + ")");
+            return obj;
+        } catch (e) {
+            return {};
+        }
+    }
+    /**
+     * 将对象或者字符串转换为json字符串
+     * @param value
+     */
+    export function jsonStringify(value: any): string {
+        if (typeof value == "string") {
+            return JSON.stringify(JSON.parse(value));
+        }
+        return JSON.stringify(value);
+    }
 }
